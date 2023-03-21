6fe4d0f188d1e5e7525eda76860fafb16e42af21
Agustin Vera <ronedev25@gmail.com>
2023-03-21 19:58:09 -0300

    add: useModal hook and AddModal component to add tasks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
+import AddModal from "./components/AddModal";
 import { TodoContext } from "./context/tasksContext";
+import useModal from "./hook/useModal";
 
 function App() {
   const todosContext = React.useContext(TodoContext);
+  const {isOpened, toggle} = useModal()
   console.log(todosContext);
   return (
     <div className="App">
+      {isOpened && <AddModal />}
       <div style={{ display:"flex", justifyContent:"space-between", alignItems:"center"}}>
         <h2>Todo list</h2>
-        <button>Add task</button>
+        <button onClick={toggle}>Add task</button>
       </div>
       <ul>
         {todosContext?.todos.length ? (
---
+++ client/src/components/AddModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddModal = ()=>{
+    return(
+        <div className='modal'>
+            <h2>Add task</h2>
+        </div>
+    )
+}
+
+export default AddModal
